2d2ad555f8db9baa2beccc3b419366bf4bd438fc
Rishabh  Malewar <rishabh.malewar.20.1@mountblue.tech>
2022-12-19 13:28:31 +0530

    Added APIs for CheckItems

--- src/Api.jsx
+++ src/Api.jsx
@@ -52,3 +52,19 @@ export function createChecklist(id, name){
 export function deleteChecklist(id){
     return axios.delete(`https://api.trello.com/1/checklists/${id}?key=857433641f31e33014fd315ea42cdb2a&token=ATTA54d866403fc3ceb960638e84ecbe6027fa6d870cf1c996afee6f26b24451e2fdD8FC6562`)
 }
+
+export function getCheckItems(id){
+    return axios.get(`https://api.trello.com/1/checklists/${id}/checkItems?key=857433641f31e33014fd315ea42cdb2a&token=ATTA54d866403fc3ceb960638e84ecbe6027fa6d870cf1c996afee6f26b24451e2fdD8FC6562`)
+}
+
+export function addCheckItems(id, name){
+    return axios.post(`https://api.trello.com/1/checklists/${id}/checkItems?name=${name}&key=857433641f31e33014fd315ea42cdb2a&token=ATTA54d866403fc3ceb960638e84ecbe6027fa6d870cf1c996afee6f26b24451e2fdD8FC6562`)
+}
+
+export function deleteItem(id, idCheckItem){
+    return axios.delete(`https://api.trello.com/1/checklists/${id}/checkItems/${idCheckItem}?key=857433641f31e33014fd315ea42cdb2a&token=ATTA54d866403fc3ceb960638e84ecbe6027fa6d870cf1c996afee6f26b24451e2fdD8FC6562`)
+}
+
+export function checkItemStatus(id, idCheckItem, status){
+    return axios.put(`https://api.trello.com/1/cards/${id}/checkItem/${idCheckItem}?state=${status}&key=857433641f31e33014fd315ea42cdb2a&token=ATTA54d866403fc3ceb960638e84ecbe6027fa6d870cf1c996afee6f26b24451e2fdD8FC6562`)
+}
